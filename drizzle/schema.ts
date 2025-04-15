035ee33c8e1fbeec45ca6dcf688a22a219dccd7c
Yash Tambi <tambiyash@gmail.com>
2025-04-15 20:25:17 +0530

    Initial commit for backend code. Initialised with drizzle, postgres, fastify, jwt.

---
+++ drizzle/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  password: text('password'),
+  created_at: timestamp('created_at').defaultNow(),
+});
