98e31e497a360eef23d0f2bd8e7b876e65069e6b
Claudio Fuentes <imclaudfuen@gmail.com>
2025-04-23 16:16:14 -0400

    Update drizzle configuration and add initial SQL migration scripts
    - Updated drizzle.config.js to specify output directory for generated files.
    - Added new SQL migration script for creating tables: account, generations, payments, session, user, and verification, along with their respective foreign key constraints.
    - Introduced metadata files for migration tracking and versioning.

--- drizzle.config.js
+++ drizzle.config.js
@@ -12,7 +12,7 @@ if (!process.env.DATABASE_URL) {
 export default defineConfig({
 	dialect: "postgresql",
 	schema: "./src/db/schema.ts",
-
+	out: "./drizzle",
 	dbCredentials: {
 		url: process.env.DATABASE_URL,
 	},
